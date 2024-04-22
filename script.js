df60a7f3fa4ed96d2b54c642a4df2c43a374b9c2
Daniel Turner <lunartechfreek@googlemail.com>
2024-04-22 16:31:30 +0000

    Complete lesson

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
  
 
- 	$("#stream1_btn").on("click", function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
+ 	$("#stream1_btn").click(function() {
+ 		$(".stream1").slideToggle(3000);;
 	});
 	$("#stream2_btn").on("click", function() {
 		$(".stream1").removeClass('highlight_stream');
